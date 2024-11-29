68580154be8e02d48ea7d66869e17aa75f55a0c1
Mohammed Zeggaf <mo.zeggaf@gmail.com>
2024-11-29 16:55:12 +0100

    added a alias to see logtime

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@ source .venv/bin/activate
 pip3 install -r requirements.txt
 echo 'do shell script "'$VIRTUAL_ENV'/bin/python3 '$PWD'/logtime.py"' > logtime.applescript
 osacompile -o logtime.app logtime.applescript
-deactivate
 STARTUP_SCRIPT='tell application "System Events" to make login item at end with properties {path:"'$PWD'/logtime.app", hidden:false}'
 echo $STARTUP_SCRIPT
 osascript -e "$STARTUP_SCRIPT"
 open -n -g logtime.app
+echo "alias logtime='$VIRTUAL_ENV/bin/python3 $PWD/get_logtime.py'" >> ~/.bash_profile
+echo "alias logtime='$VIRTUAL_ENV/bin/python3 $PWD/get_logtime.py'" >> ~/.zshrc
+source ~/.bash_profile
+source ~/.zshrc
 echo 'Installation complete. '
