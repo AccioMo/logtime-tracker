7843a7f276c939f9c0aedc7f2c0b4418263131d8
Mohammed Zeggaf <mo.zeggaf@gmail.com>
2024-11-29 16:50:01 +0100

    launch appa in background

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
-echo 'do shell script "$VIRTUAL_ENV/bin/python3 '$VIRTUAL_ENV'/logtime.py"' > logtime.applescript
+echo 'do shell script "'$VIRTUAL_ENV'/bin/python3 '$PWD'/logtime.py"' > logtime.applescript
 osacompile -o logtime.app logtime.applescript
+deactivate
 STARTUP_SCRIPT='tell application "System Events" to make login item at end with properties {path:"'$PWD'/logtime.app", hidden:false}'
+echo $STARTUP_SCRIPT
 osascript -e "$STARTUP_SCRIPT"
+open -n -g logtime.app
+echo 'Installation complete. '
